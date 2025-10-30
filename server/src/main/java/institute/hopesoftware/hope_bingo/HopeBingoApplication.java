25fb50eb06f9d9c695e2bc3585e98f1ccedba863
Matthew stowe <matthew.stowe@hope.edu>
2025-10-30 09:57:58 -0400

    add starting code for spring framework

---
+++ server/src/main/java/institute/hopesoftware/hope_bingo/HopeBingoApplication.java
@@ -0,0 +1,13 @@
+package institute.hopesoftware.hope_bingo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HopeBingoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HopeBingoApplication.class, args);
+	}
+
+}
---
+++ server/src/test/java/institute/hopesoftware/hope_bingo/HopeBingoApplicationTests.java
@@ -0,0 +1,13 @@
+package institute.hopesoftware.hope_bingo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HopeBingoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
