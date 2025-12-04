cb3c5e8935a5aeac6a7ea4155d2b3e53e8226317
Samuel <samuellorenbuck@gmail.com>
2025-12-04 10:01:34 -0500

    board service and controller hopefully done

--- server/src/main/java/institute/hopesoftware/hope_bingo/services/BoardService.java
+++ server/src/main/java/institute/hopesoftware/hope_bingo/services/BoardService.java
@@ -1,23 +1,52 @@
 package institute.hopesoftware.hope_bingo.services;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.Set;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import institute.hopesoftware.hope_bingo.model.Board;
-
+import institute.hopesoftware.hope_bingo.model.Question;
+import institute.hopesoftware.hope_bingo.repositories.BoardRepository;
 
 @Service
 public class BoardService {
 
-    public Board newBoard(List<String> questions, Boolean private1, String gameTitle) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'newGame'");
+    private final BoardRepository boardRepository;
+
+    @Autowired
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    public Board createNewBoard(Set<Question> questions, String gameTitle, String author) {
+        Board board = new Board();
+        board.setBoardName(gameTitle);
+
+        if (author == null || author.isBlank()) {
+            board.setBoardAuthor("author");
+        } else {
+            board.setBoardAuthor(author);
+        }
+
+        board.setQuestions(questions);
+
+        return boardRepository.save(board);
     }
 
     public List<Board> getBoards() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBoards'");
+        List<Board> boards = new ArrayList<>();
+        boardRepository.findAll().forEach(boards::add);
+        return boards;
+    }
+    public Board findBoardByName(String name) {
+        return boardRepository.findByBoardName(name);
     }
     
-}
+    public List<Board> findAllBoardsByName(String name) {
+        return boardRepository.findAllByBoardAuthor(name);
+    }
+    }
